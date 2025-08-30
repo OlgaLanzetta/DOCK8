799db24b332376928164fd067013cf0137fd6e18
OlgaLanzetta <106872836+OlgaLanzetta@users.noreply.github.com>
2025-08-30 06:23:48 +0200

    Update 3_Plots.R

--- script/3_Plots.R
+++ script/3_Plots.R
@@ -136,6 +136,109 @@ unique_colors <- c(
  DimPlot(integrated, reduction = "umap_harmony", group.by = "harmony_0.6", cols = unique_colors, label = T, pt.size = 0.7, label.size = 10)
 
 # Panel B
+# load the integrated object
+# Define custom colors for each cluster
+unique_colors <- c(
+  "0"  = "#619CFF", "1"  = "#33a02c", "2"  = "#984ea3", "3"  = "#ff7f00", "4"  = "#fb9a99",
+  "5"  = "#a65628", "6"  = "#E76BF3", "7"  = "#999999", "8"  = "#dede00", "9"  = "darkslateblue",
+  "10" = "gold",     "11" = "#00BCD8", "12" = "#e41a1c", "13" = "#cab2d6", "14" = "#00FF00",
+  "15" = "#c49c94", "16" = "#0000FF", "17" = "#00FFFF", "18" = "#911EB4", "19" = "#fdb462"
+)
+
+# Set default assay
+DefaultAssay(integrated) <- "RNA"
+
+# Marker genes to visualize
+phenotype_TF_marker <- c(
+  "Cd3e", "Cd3d", "Cd3g", "Cd8a", "Cd4", "Trgv1", "Cd19", "Ms4a1", "Ncr1", "H2-Ab1",
+  "H2-Eb2", "Klrb1", "Itgam", "Itgax", "Cd14", "Lyz2", "Il7r", "Itgae", "Cd69", "Pdcd1",
+  "Foxp3", "Eomes", "Tbx21", "Gata3", "Rorc", "Rora", "Il23r"
+)
+
+
+# Extract expression and metadata
+df <- FetchData(integrated, vars = c("harmony_0.6", phenotype_TF_marker)) %>%
+  mutate(cell = rownames(.)) %>%
+  pivot_longer(cols = all_of(phenotype_TF_marker), names_to = "gene", values_to = "expression")
+
+# Ensure harmony_0.6 is a factor with proper levels
+
+cluster_levels <- as.character(sort(as.numeric(unique(df$harmony_0.6))))
+
+df$harmony_0.6 <- factor(df$harmony_0.6)
+df$gene <- factor(df$gene, levels = phenotype_TF_marker)
+
+# Match color to clusters
+stopifnot(length(cluster_levels) <= length(unique_colors))
+cluster_colors <- setNames(unique_colors[seq_along(cluster_levels)], cluster_levels)
+
+# Save as high-resolution TIFF
+tiff("phenotype_TF_marker.tiff", width = 8, height = 12, units = "in", res = 600)
+
+# Plot
+p <- ggplot(df, aes(x = harmony_0.6, y = expression, fill = harmony_0.6)) +
+  geom_violin(scale = "width", trim = TRUE, adjust = 1, width = 1) +
+  geom_jitter(size = 0.01, width = 0.25, alpha = 0.2) +
+  facet_wrap(~gene, nrow = length(phenotype_TF_marker), strip.position = "right", scales = "free_y") +
+  scale_fill_manual(values = cluster_colors) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(size = 10, angle = 0),
+    axis.text.y = element_text(size = 8),
+    axis.title = element_blank(),
+    strip.text.y.right = element_text(angle = 0, size = 10, face = "italic"),
+    strip.placement = "outside",
+    legend.position = "none",
+    panel.grid = element_blank()
+  )
+
+print(p)
+dev.off()
+
+#######################
+
+cytokines <- c("Ifng", "Il4", "Il5", "Il17a", "Il22", "Tnf", "Il10", "Il21",
+               "Gzma", "Gzmb", "Fasl", "Prf1", "Tgfb1", "Il1b", "Ccl3", "Ccl4")
+
+# Extract expression and metadata
+df <- FetchData(integrated, vars = c("harmony_0.6", cytokines)) %>%
+  mutate(cell = rownames(.)) %>%
+  pivot_longer(cols = all_of(cytokines), names_to = "gene", values_to = "expression")
+
+# Ensure harmony_0.6 is a factor with proper levels
+
+cluster_levels <- as.character(sort(as.numeric(unique(df$harmony_0.6))))
+
+df$harmony_0.6 <- factor(df$harmony_0.6)
+df$gene <- factor(df$gene, levels = cytokines)
+
+# Match color to clusters
+stopifnot(length(cluster_levels) <= length(unique_colors))
+cluster_colors <- setNames(unique_colors[seq_along(cluster_levels)], cluster_levels)
+
+# Save as high-resolution TIFF
+tiff("cytokines.tiff", width = 8, height = 12, units = "in", res = 600)
+
+# Plot
+p <- ggplot(df, aes(x = harmony_0.6, y = expression, fill = harmony_0.6)) +
+  geom_violin(scale = "width", trim = TRUE, adjust = 1, width = 1) +
+  geom_jitter(size = 0.01, width = 0.25, alpha = 0.2) +
+  facet_wrap(~gene, nrow = length(cytokines), strip.position = "right", scales = "free_y") +
+  scale_fill_manual(values = cluster_colors) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(size = 10, angle = 0),
+    axis.text.y = element_text(size = 8),
+    axis.title = element_blank(),
+    strip.text.y.right = element_text(angle = 0, size = 10, face = "italic"),
+    strip.placement = "outside",
+    legend.position = "none",
+    panel.grid = element_blank()
+  )
+
+print(p)
+dev.off()
+
 
 # Panel C
 # load the integrated object
